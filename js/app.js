d0759afd1189ee894cdff4fc3b23a727de1119ef
msjtr <msjt301@gmail.com>
2026-03-25 00:40:52 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-
 const packages = [
-  {id:1,cards:25,total:500,down:125,fees:125,net:250},
-  {id:2,cards:50,total:1000,down:250,fees:250,net:500},
-  {id:3,cards:75,total:1500,down:375,fees:325,net:800},
-  {id:4,cards:94,total:1880,down:470,fees:410,net:1000},
-  {id:5,cards:100,total:2000,down:500,fees:450,net:1050},
-  {id:6,cards:125,total:2500,down:625,fees:575,net:1300}
+  {id:1,total:500,net:250},
+  {id:2,total:1000,net:500},
+  {id:3,total:1500,net:800},
+  {id:4,total:1880,net:1000},
+  {id:5,total:2000,net:1050},
+  {id:6,total:2500,net:1300}
 ];
 
 let selectedPackage = null;
@@ -17,24 +16,44 @@ document.getElementById("packageSelect").addEventListener("change", function(){
   selectedPackage = pkg;
 
   document.getElementById("result").innerHTML = `
-    <p>عدد البطاقات: ${pkg.cards}</p>
     <p>القيمة: ${pkg.total} ريال</p>
-    <p>الدفعة الأولى: ${pkg.down} ريال</p>
-    <p>الرسوم: ${pkg.fees} ريال</p>
-    <hr>
     <p class="net">الربح: ${pkg.net} ريال</p>
   `;
 });
 
-function sendWhatsApp() {
-  const name = document.getElementById("name").value;
-  const phone = document.getElementById("phoneInput").value;
+function confirmOrder() {
 
-  if (!name || !phone || !selectedPackage) {
+  const first = document.getElementById("firstName").value;
+  const second = document.getElementById("secondName").value;
+  const third = document.getElementById("thirdName").value;
+  const phone = document.getElementById("phone").value;
+
+  if (!first || !second || !third || !phone || !selectedPackage) {
     alert("أكمل البيانات");
     return;
   }
 
+  const fullName = `${first} ${second} ${third}`;
+
+  if (!confirm("هل ترغب بإرسال الطلب؟")) return;
+
+  generatePDF(fullName, phone);
+
+  setTimeout(() => {
+    sendWhatsApp(fullName, phone);
+  }, 3000);
+}
+
+function cancelOrder() {
+  location.reload();
+}
+
+function generatePDF(name, phone) {
+  const element = document.querySelector(".card");
+  html2pdf().from(element).save("tera-order.pdf");
+}
+
+function sendWhatsApp(name, phone) {
   const msg = `
 طلب جديد
 
@@ -47,10 +66,4 @@ function sendWhatsApp() {
   `;
 
   window.open("https://wa.me/966555698774?text=" + encodeURIComponent(msg));
-}
-
-function generatePDF() {
-  const element = document.querySelector(".card");
-
-  html2pdf().from(element).save("tera-order.pdf");
 }
